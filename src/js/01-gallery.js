67096baf247b7247dd0c1f3f8dd31bc785ce7722
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-02-28 11:46:21 +0200

    refactor code: add import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const makeGalleryHtmlMarkup = (imgs) => {
+  return imgs.map(({preview, original, description}) => {
+    return `
+                  <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                  </a>
+               `
+  }).join("");
+}
+
+const insertMarkup = (ref, markupStr) => {
+  ref.innerHTML = markupStr;
+}
+
+insertMarkup(galleryRef, makeGalleryHtmlMarkup(galleryItems));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
