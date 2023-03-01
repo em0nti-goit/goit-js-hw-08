15f5cf3086c6dda8a943d89e50dade518ce122a4
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-03-01 11:18:07 +0200

    complete task #2: set time on load page

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const TIME_UPDATE_KEY = 'videoplayer-current-time';
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Player(iframeRef);
+let dataTimeInSeconds = 0;
+
+try {
+  const data = JSON.parse(localStorage.getItem(TIME_UPDATE_KEY));
+  dataTimeInSeconds = data.seconds;
+  console.log('in try construction');
+} catch (e) {
+  console.log(e.message);
+  // dataTimeInSeconds = 0;
+}
+
+// noinspection JSUnresolvedFunction
+player.setCurrentTime(dataTimeInSeconds).then(function (seconds) {
+  console.log(seconds);
+})
+
+player.on('timeupdate', (data) => {
+  localStorage.setItem(TIME_UPDATE_KEY, JSON.stringify(data));
+})
